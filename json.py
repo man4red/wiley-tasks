37bdad3981e2fb98555efe0566e4ab7733adb378
man4red <man4red@users.noreply.github.com>
2019-07-03 03:06:08 +0300

    Update json.py

--- json.py
+++ json.py
@@ -67,17 +67,17 @@ result = json.loads(jsonstr)
 #print('dump:', json.dumps(result, indent=4))
 
 def main():
-	""" fetch the user’s friend’s name those “id” equals “2”, and has ‘laborum’ tag
-	"""
+  """ fetch the user’s friend’s name those “id” equals “2”, and has ‘laborum’ tag
+  """
 
-	for row in result:
-		try:
-			if 'laborum' in row['tags']:
-				for friend in row['friends']:
-					if friend.get('id') == 2:
-						print(friend.get('name'))
-		except KeyError:
-			pass
-		
+  for row in result:
+    try:
+      if 'laborum' in row['tags']:
+        for friend in row['friends']:
+          if friend.get('id') == 2:
+            print(friend.get('name'))
+    except KeyError:
+      pass
+    
 if __name__ == "__main__":
     main()
