533372704a1cb3f0b6df9fc21b0f33631c1a89f2
man4red <man4red@users.noreply.github.com>
2019-07-03 14:38:26 +0300

    Update wall.py

--- wall.py
+++ wall.py
@@ -17,21 +17,27 @@ logger = logging.getLogger(__name__)
 
 ssh = paramiko.SSHClient()
 
+
 def main():
-  hosts = ["qaserver1", "qaserver2", "qaserver3", "qaserver4", "qaserver5", "qaserver6"]
-  username = "user"
-  password = "password"
-  #key = paramiko.RSAKey.from_private_key_file(keyfilename)
-  #key = paramiko.DSSKey.from_private_key_file(keyfilename)
-  for host in hosts:
-    try:
-      ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-      ssh.connect(host, username=username, password=password, timeout=2)
-      ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command("wall $(cat /proc/cmdline)")
-      logger.info("Server %s - command was sent", host)
-    except:
-      logger.error("Server %s is not available or wrong user/pass", host)
-      pass
+    hosts = [
+        "qaserver1", "qaserver2", "qaserver3", "qaserver4", "qaserver5",
+        "qaserver6"
+    ]
+    username = "user"
+    password = "password"
+    #key = paramiko.RSAKey.from_private_key_file(keyfilename)
+    #key = paramiko.DSSKey.from_private_key_file(keyfilename)
+    for host in hosts:
+        try:
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            ssh.connect(host, username=username, password=password, timeout=2)
+            ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(
+                "wall $(cat /proc/cmdline)")
+            logger.info("Server %s - command was sent", host)
+        except:
+            logger.error("Server %s is not available or wrong user/pass", host)
+            pass
+
 
 if __name__ == "__main__":
     main()
